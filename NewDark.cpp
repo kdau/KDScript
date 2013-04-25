89c9b9f8ca2ada274c04a5a88141e24c40eaf610
Kevin Daughtridge <kevin@kdau.com>
2013-04-25 00:47:00 -0700

    First draft of NewDark scripts.

--- NewDark.cpp
+++ NewDark.cpp
@@ -24,3 +24,318 @@
 #include <ScriptLib.h>
 #include "utils.h"
 
+#include <windows.h>
+#undef GetClassName // ugh, Windows...
+
+
+
+/* KDGetInfo */
+
+cScr_GetInfo::cScr_GetInfo (const char* pszName, int iHostObjId)
+	: cBaseScript (pszName, iHostObjId)
+{}
+
+long
+cScr_GetInfo::OnBeginScript (sScrMsg*, cMultiParm&)
+{
+	UpdateVariables ();
+	return 0;
+}
+
+long
+cScr_GetInfo::OnDarkGameModeChange (sDarkGameModeScrMsg* pMsg, cMultiParm&)
+{
+	if (pMsg->fResuming)
+		UpdateVariables ();
+	return 0;
+}
+
+void
+cScr_GetInfo::UpdateVariables ()
+{
+	DEBUG_STRING ("updating info_* quest variables");
+
+	SService<IDarkGameSrv> pDGS (g_pScriptManager);
+	SService<IEngineSrv> pES (g_pScriptManager);
+	SService<IQuestSrv> pQS (g_pScriptManager);
+	SService<IVersionSrv> pVS (g_pScriptManager);
+
+	pQS->Set ("info_directx_version", pES->IsRunningDX6 () ? 6 : 9,
+		kQuestDataMission);
+
+	int display_height = 0, display_width = 0;
+	pES->GetCanvasSize (display_width, display_height);
+	pQS->Set ("info_display_height", display_height, kQuestDataMission);
+	pQS->Set ("info_display_width", display_width, kQuestDataMission);
+
+	int value = 0;
+	if (pES->ConfigGetInt ("sfx_eax", value))
+		pQS->Set ("info_has_eax", value, kQuestDataMission);
+	if (pES->ConfigGetInt ("fogging", value))
+		pQS->Set ("info_has_fog", value, kQuestDataMission);
+	if (pES->ConfigGetInt ("game_hardware", value))
+		pQS->Set ("info_has_hw3d", value, kQuestDataMission);
+	if (pES->ConfigGetInt ("enhanced_sky", value))
+		pQS->Set ("info_has_sky", value, kQuestDataMission);
+	if (pES->ConfigGetInt ("render_weather", value))
+		pQS->Set ("info_has_weather", value, kQuestDataMission);
+
+	pQS->Set ("info_mission", pDGS->GetCurrentMission (), kQuestDataMission);
+
+	pQS->Set ("info_mode", pVS->IsEditor (), kQuestDataMission);
+
+	int version_major = 0, version_minor = 0;
+	pVS->GetVersion (version_major, version_minor);
+	pQS->Set ("info_version_major", version_major, kQuestDataMission);
+	pQS->Set ("info_version_minor", version_minor, kQuestDataMission);
+}
+
+
+
+/* KDTrapEnvMapTexture */
+
+cScr_TrapEnvMapTexture::cScr_TrapEnvMapTexture
+		(const char* pszName, int iHostObjId)
+	: cBaseScript (pszName, iHostObjId),
+	  cBaseTrap (pszName, iHostObjId)
+{}
+
+long
+cScr_TrapEnvMapTexture::OnSwitch (bool bState, sScrMsg*, cMultiParm&)
+{
+	int zone = GetObjectParamInt (ObjId (), "env_map_zone", -1);
+	char* texture = GetObjectParamString (ObjId (),
+		bState ? "env_map_on" : "env_map_off", NULL);
+
+	if (zone < 0 || zone > 63 || !texture) return 1;
+
+	DEBUG_PRINTF ("setting cubemap texture for enviroment zone %d to `%s'",
+		zone, texture);
+
+	SService<IEngineSrv> pES (g_pScriptManager);
+	pES->SetEnvMapZone (zone, texture);
+	g_pMalloc->Free (texture);
+
+	return 0;
+}
+
+
+
+/* KDTrapFog */
+
+cScr_TrapFog::cScr_TrapFog (const char* pszName, int iHostObjId)
+	: cBaseScript (pszName, iHostObjId),
+	  cBaseTrap (pszName, iHostObjId),
+	  SCRIPT_VAROBJ (TrapFog, timer, iHostObjId),
+	  SCRIPT_VAROBJ (TrapFog, zone, iHostObjId),
+	  SCRIPT_VAROBJ (TrapFog, time_total, iHostObjId),
+	  SCRIPT_VAROBJ (TrapFog, start_red, iHostObjId),
+	  SCRIPT_VAROBJ (TrapFog, start_green, iHostObjId),
+	  SCRIPT_VAROBJ (TrapFog, start_blue, iHostObjId),
+	  SCRIPT_VAROBJ (TrapFog, end_red, iHostObjId),
+	  SCRIPT_VAROBJ (TrapFog, end_green, iHostObjId),
+	  SCRIPT_VAROBJ (TrapFog, end_blue, iHostObjId),
+	  SCRIPT_VAROBJ (TrapFog, start_dist, iHostObjId),
+	  SCRIPT_VAROBJ (TrapFog, end_dist, iHostObjId)
+{}
+
+long
+cScr_TrapFog::OnSwitch (bool bState, sScrMsg*, cMultiParm&)
+{
+	if (timer.Valid ()) // stop any previous transition
+	{
+		KillTimedMessage (timer);
+		timer.Clear ();
+	}
+
+	zone = GetObjectParamInt (ObjId (), "fog_zone", -1);
+	time_total = GetObjectParamTime (ObjId (), "fog_time", 0);
+	int end_color = GetObjectParamColor (ObjId (),
+		bState ? "fog_color_on" : "fog_color_off", -1);
+	end_dist = GetObjectParamFloat (ObjId (),
+		bState ? "fog_dist_on" : "fog_dist_off", -1.0);
+
+	if (zone < 0 || zone > 8 || end_color < 0 || end_dist < 0.0) return 1;
+
+	end_red = GetRValue (end_color),
+	end_green = GetGValue (end_color),
+	end_blue = GetBValue (end_color);
+
+	DEBUG_PRINTF ("setting fog for zone %d to RGB %d,%d,%d at distance %f over %d ms",
+		(int) zone, (int) end_red, (int) end_green, (int) end_blue,
+		(float) end_dist, (int) time_total);
+
+	int sr, sg, sb; float sd;
+	SService<IEngineSrv> pES (g_pScriptManager);
+	if (zone == 0)
+		pES->GetFog (sr, sg, sb, sd);
+	else
+		pES->GetFogZone (zone, sr, sg, sb, sd);
+	start_red = sr;
+	start_green = sg;
+	start_blue = sb;
+	start_dist = sd;
+
+	IncrementFog (time_total);
+
+	return 0;
+}
+
+long
+cScr_TrapFog::OnTimer (sScrTimerMsg* pMsg, cMultiParm& mpReply)
+{
+	if (!strcmp (pMsg->name, "IncrementFog"))
+	{
+		if (timer.Valid ()) timer.Clear (); // one-shot
+		IncrementFog (pMsg->data.type == kMT_Int
+			? (int) pMsg->data : 0);
+	}
+
+	return cBaseTrap::OnTimer (pMsg, mpReply);
+}
+
+void
+cScr_TrapFog::IncrementFog (int time_remaining)
+{
+	time_remaining =
+		std::max (0, std::min ((int) time_total, time_remaining));
+	if (time_remaining > 0)
+		timer = SetTimedMessage ("IncrementFog", 50, kSTM_OneShot,
+			time_remaining - 50);
+
+	float progress = (time_total - time_remaining) / time_total;
+
+	int red = start_red + progress * (end_red - start_red),
+		green = start_green + progress * (end_green - start_green),
+		blue = start_blue + progress * (end_blue - start_blue);
+
+	float fake_end_dist = (end_dist == 0.0 && progress < 1.0)
+		? 100000.0 : end_dist;
+	float dist = start_dist + progress * (fake_end_dist - start_dist);
+
+	SService<IEngineSrv> pES (g_pScriptManager);
+	if (zone == 0)
+		pES->SetFog (red, green, blue, dist);
+	else
+		pES->SetFogZone (zone, red, green, blue, dist);
+}
+
+
+
+/* KDTrapNextMission */
+
+cScr_TrapNextMission::cScr_TrapNextMission (const char* pszName, int iHostObjId)
+	: cBaseScript (pszName, iHostObjId),
+	  cBaseTrap (pszName, iHostObjId)
+{}
+
+long
+cScr_TrapNextMission::OnSwitch (bool bState, sScrMsg*, cMultiParm&)
+{
+	int next_mission = GetObjectParamInt (ObjId (),
+		bState ? "next_mission_on" : "next_mission_off", 0);
+
+	if (next_mission < 1) return 1;
+
+	DEBUG_PRINTF ("setting next mission to %d", next_mission);
+
+	SService<IDarkGameSrv> pDGS (g_pScriptManager);
+	pDGS->SetNextMission (next_mission);
+
+	return 0;
+}
+
+
+
+/* DarkWeather */
+
+struct DarkWeather
+{
+	DarkWeather ();
+	void SetFromMission ();
+	void ApplyToMission () const;
+
+	int precip_type;
+	float precip_freq;
+	float precip_speed;
+	float vis_dist;
+	float rend_radius;
+	float alpha;
+	float brightness;
+	float snow_jitter;
+	float rain_length;
+	float splash_freq;
+	float splash_radius;
+	float splash_height;
+	float splash_duration;
+	cScrStr texture;
+	cScrVec wind;
+};
+
+DarkWeather::DarkWeather ()
+	: precip_type (0), precip_freq (0.0), precip_speed (0.0), vis_dist (0.0),
+	  rend_radius (0.0), alpha (0.0), brightness (0.0), snow_jitter (0.0),
+	  rain_length (0.0), splash_freq (0.0), splash_radius (0.0),
+	  splash_height (0.0), splash_duration (0.0), texture (),
+	  wind (0.0, 0.0, 0.0)
+{
+	SetFromMission ();
+}
+
+void
+DarkWeather::SetFromMission ()
+{
+	SService<IEngineSrv> pES (g_pScriptManager);
+	pES->GetWeather (precip_type, precip_freq, precip_speed, vis_dist,
+		rend_radius, alpha, brightness, snow_jitter, rain_length,
+		splash_freq, splash_radius, splash_height, splash_duration,
+		texture, wind);
+}
+
+void
+DarkWeather::ApplyToMission () const
+{
+	SService<IEngineSrv> pES (g_pScriptManager);
+	pES->SetWeather (precip_type, precip_freq, precip_speed, vis_dist,
+		rend_radius, alpha, brightness, snow_jitter, rain_length,
+		splash_freq, splash_radius, splash_height, splash_duration,
+		texture, wind);
+}
+
+
+
+/* KDTrapWeather */
+
+cScr_TrapWeather::cScr_TrapWeather (const char* pszName, int iHostObjId)
+	: cBaseScript (pszName, iHostObjId),
+	  cBaseTrap (pszName, iHostObjId)
+{}
+
+long
+cScr_TrapWeather::OnSwitch (bool bState, sScrMsg*, cMultiParm&)
+{
+	float freq = GetObjectParamFloat (ObjId (),
+		bState ? "precip_freq_on" : "precip_freq_off", -1.0);
+	float speed = GetObjectParamFloat (ObjId (),
+		bState ? "precip_speed_on" : "precip_speed_off", -1.0);
+
+	if (freq < 0.0 && speed < 0.0) return 1; // either may be set
+
+	DarkWeather weather;
+
+	if (freq >= 0.0)
+	{
+		DEBUG_PRINTF ("setting precipitation frequency to %f", freq);
+		weather.precip_freq = freq;
+	}
+
+	if (speed >= 0.0)
+	{
+		DEBUG_PRINTF ("setting precipitation speed to %f", speed);
+		weather.precip_speed = speed;
+	}
+
+	weather.ApplyToMission ();
+	return 0;
+}
+
--- NewDark.h
+++ NewDark.h
@@ -25,8 +25,210 @@
 
 #if !SCR_GENSCRIPTS
 #include "BaseScript.h"
+#include "BaseTrap.h"
 #include "scriptvars.h"
 #endif // SCR_GENSCRIPTS
+
+
+
+/**
+ * Script: KDGetInfo
+ * Inherits: BaseScript
+ *
+ * Every time the mission is loaded or game mode is entered, populates a number
+ * of mission quest variables with information about the game environment. These
+ * values will be available at Sim time, and can also be tracked by TrigQVar.
+ *
+ *     info_directx_version: the DirectX version in use (6 = old, 9 = new)
+ *     info_display_height: the height of the drawing area (screen/window)
+ *     info_display_width: the height of the drawing area (screen/window)
+ *         (e.g. fullscreen 1920x1080 is height 1080, width 1920)
+ *     info_has_eax: whether EAX is enabled (1 = yes, 0 = no)
+ *     info_has_fog: whether fogging is enabled (1 = yes, 0 = no)
+ *     info_has_hw3d: whether hardware 3D rendering is enabled (1 = yes, 0 = no)
+ *     info_has_sky: whether "enhanced" sky is enabled (1 = yes, 0 = no)
+ *     info_has_weather: whether weather is enabled (1 = yes, 0 = no)
+ *     info_mission: the number of the current mission (e.g. miss20 = 20)
+ *     info_mode: the current engine mode:
+ *         0 = regular game mode (e.g. thief2.exe)
+ *         1 = editor (e.g. dromed.exe) in edit mode
+ *         2 = editor in game mode
+ *     info_version_major: the major component of the engine version
+ *     info_version_minor: the minor component of the engine version
+ *         (e.g. version 1.21 is major 1, minor 21)
+ */
+#if !SCR_GENSCRIPTS
+class cScr_GetInfo : public virtual cBaseScript
+{
+public:
+	cScr_GetInfo (const char* pszName, int iHostObjId);
+
+protected:
+	virtual long OnBeginScript (sScrMsg* pMsg, cMultiParm& mpReply);
+	virtual long OnDarkGameModeChange (sDarkGameModeScrMsg* pMsg,
+		cMultiParm& mpReply);
+
+private:
+	void UpdateVariables ();
+};
+#else // SCR_GENSCRIPTS
+GEN_FACTORY("KDGetInfo","BaseScript",cScr_GetInfo)
+#endif // SCR_GENSCRIPTS
+
+
+
+/**
+ * Script: KDTrapEnvMapTexture
+ * Inherits: BaseTrap
+ * Parameter: env_zone (integer) - The zone to change the texture for (0-63).
+ * Parameter: env_map_on (string) - The texture to set when turned on.
+ * Parameter: env_map_off (string) - The texture to set when turned off.
+ *
+ * When triggered, changes the cubemap texture associated with an environment
+ * zone. If the env_zone parameter is 0, the global texture is changed. (The
+ * default textures, both global and per zone, are defined in the Mission
+ * Variables -> Environment Maps dialog. See the NewDark documentation for more
+ * details on environment maps and reflective materials.)
+ *
+ * When turned on, sets the texture for the specified zone to the value of the
+ * env_map_on parameter, if defined; when turned off, sets it to env_map_off.
+ * Values should be specified as relative paths, e.g. "tex\envmaps\hallway".
+ * The usual trap control flags, locking, and timing are respected.
+ */
+#if !SCR_GENSCRIPTS
+class cScr_TrapEnvMapTexture : public virtual cBaseTrap
+{
+public:
+	cScr_TrapEnvMapTexture (const char* pszName, int iHostObjId);
+
+protected:
+	virtual long OnSwitch (bool bState, sScrMsg* pMsg, cMultiParm& mpReply);
+};
+#else // SCR_GENSCRIPTS
+GEN_FACTORY("KDTrapEnvMapTexture","BaseScript",cScr_TrapEnvMapTexture)
+#endif // SCR_GENSCRIPTS
+
+
+
+/**
+ * Script: KDTrapFog
+ * Inherits: BaseTrap
+ * Parameter: fog_zone (integer) - The zone to change the fog for (0-8).
+ * Parameter: fog_time (time) - Length of the fog change, in milliseconds.
+ * Parameter: fog_color_on (color) - The color of fog to set when turned on.
+ * Parameter: fog_dist_on (real) - The distance of fog to set when turned on.
+ * Parameter: fog_color_off (color) - The color of fog to set when turned off.
+ * Parameter: fog_dist_off (real) - The distance of fog to set when turned off.
+ *
+ * When triggered, changes the color and distance of fog for the specified fog
+ * zone. If the fog_zone parameter is 0, the global fog is changed. (The
+ * default fog, both global and per zone, is defined in the Mission Variables ->
+ * Fog Zones dialog.) The fog_time parameter specifies how long it will take to
+ * reach the new fog values; if it is set to 0 or undefined, the change will be
+ * instant.
+ *
+ * When turned on, sets the fog color and distance for the specified zone to the
+ * values of the fog_color_on and fog_dist_on parameters, if both are defined.
+ * When turned off, sets the fog to fog_color_off and fog_dist_off, if defined.
+ * If fog_dist_{on,off} is 0.0, fog is turned off completely. (Transitions to
+ * and from a lack of fog are handled correctly.) The usual trap control flags,
+ * locking, and timing (before the start of the effect, not its duration) are
+ * respected.
+ */
+#if !SCR_GENSCRIPTS
+class cScr_TrapFog : public virtual cBaseTrap
+{
+public:
+	cScr_TrapFog (const char* pszName, int iHostObjId);
+
+protected:
+	virtual long OnSwitch (bool bState, sScrMsg* pMsg, cMultiParm& mpReply);
+	virtual long OnTimer (sScrTimerMsg* pMsg, cMultiParm& mpReply);
+
+private:
+	void IncrementFog (int time_remaining);
+
+	script_handle<tScrTimer> timer;
+	script_int zone, time_total,
+		start_red, start_green, start_blue,
+		end_red, end_green, end_blue;
+	script_float start_dist, end_dist;
+};
+#else // SCR_GENSCRIPTS
+GEN_FACTORY("KDTrapFog","BaseScript",cScr_TrapFog)
+#endif // SCR_GENSCRIPTS
+
+
+
+/**
+ * Script: KDTrapNextMission
+ * Inherits: BaseTrap
+ * Parameter: next_mission_on (integer) - The mission to set when turned on.
+ * Parameter: next_mission_off (integer) - The mission to set when turned off.
+ *
+ * When triggered, changes which mission will begin after the current mission
+ * ends. (By default, the next mission is one past the current mission, or else
+ * the mission specified in <missflag.str> in "miss_N_next" [where N is the
+ * current mission].)
+ *
+ * When turned on, sets the next mission to the value of the next_mission_on
+ * parameter, if defined; when turned off, sets it to next_mission_off. The
+ * usual trap control flags, locking, and timing are respected.
+ *
+ * IMPORTANT: The engine does not perform any validation on mission numbers.
+ * If the next mission is set to a nonexistent mission, the game will crash
+ * as soon as it tries to load it.
+ */
+#if !SCR_GENSCRIPTS
+class cScr_TrapNextMission : public virtual cBaseTrap
+{
+public:
+	cScr_TrapNextMission (const char* pszName, int iHostObjId);
+
+protected:
+	virtual long OnSwitch (bool bState, sScrMsg* pMsg, cMultiParm& mpReply);
+};
+#else // SCR_GENSCRIPTS
+GEN_FACTORY("KDTrapNextMission","BaseScript",cScr_TrapNextMission)
+#endif // SCR_GENSCRIPTS
+
+
+
+/**
+ * Script: KDTrapWeather
+ * Inherits: BaseTrap
+ * Parameter: precip_freq_on (real) - The frequency to set when turned on.
+ * Parameter: precip_speed_on (real) - The speed to set when turned on.
+ * Parameter: precip_freq_off (real) - The frequency to set when turned off.
+ * Parameter: precip_speed_off (real) - The speed to set when turned off.
+ *
+ * When triggered, changes the frequency and speed of precipitation (weather).
+ * The frequency is expressed in number of new drops per second, while the speed
+ * is expressed in feet per second. Both values must be greater than or equal
+ * to zero. (The default weather, including a number of other values, is defined
+ * in the Mission Variables -> Weather dialog.)
+ *
+ * When turned on, sets the precipitation frequency and speed to the values of
+ * either or both of the precip_freq_on and precip_speed_on parameters, if
+ * defined. When turned off, sets the precipitation to precip_freq_off and
+ * precip_speed_off, if defined. If precip_freq_{on,off} is 0.0, precipitation
+ * is turned off completely. The usual trap control flags, locking, and timing
+ * are respected.
+ */
+#if !SCR_GENSCRIPTS
+class cScr_TrapWeather : public virtual cBaseTrap
+{
+public:
+	cScr_TrapWeather (const char* pszName, int iHostObjId);
+
+protected:
+	virtual long OnSwitch (bool bState, sScrMsg* pMsg, cMultiParm& mpReply);
+};
+#else // SCR_GENSCRIPTS
+GEN_FACTORY("KDTrapWeather","BaseScript",cScr_TrapWeather)
+#endif // SCR_GENSCRIPTS
+
+
 
 #endif // NEWDARK_H
 
